ebcac68116e5db6e3f364726a8b38658ebc3cb9b
ZynoXx-777 <hkkzino@gmail.com>
2026-01-22 23:27:33 +0100

    Update settings.py

--- sulpak/settings.py
+++ sulpak/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import sulpak
-import django_heroku
+#import django_heroku
 import os 
 
 
@@ -187,6 +187,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR / 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-django_heroku.settings(locals())
+#django_heroku.settings(locals())
 
 AUTH_USER_MODEL = 'users.User'
